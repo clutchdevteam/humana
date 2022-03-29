1497ac3cc9522902f657371259bb16248ef0ce3d
Drew <drewclementsdesign@gmail.com>
2022-03-28 22:08:22 -0400

    remove storybook

--- .nuxt-storybook/composition-api/globals.mjs
+++
@@ -1,5 +0,0 @@
-export const isFullStatic = false
-export const staticPath = "/Users/drewclements/Desktop/clutch/humana/.nuxt-storybook/static-json"
-export const publicPath = "/"
-export const globalContext = "__NUXT__"
-export const globalNuxt = "$nuxt"
